6489b5affb1d426905d089af34bceeddcfab05a3
llezcano <leandro.c.lezcano@gmail.com>
2014-07-27 22:55:09 -0300

    agregado project tracking facade

--- src/main/java/org/tesys/core/project/tracking/ProjectTracking.java
+++ src/main/java/org/tesys/core/project/tracking/ProjectTracking.java
@@ -1,5 +1,21 @@
 package org.tesys.core.project.tracking;
 
-public class ProjectTracking {
+/**
+ *  Interface para un facade de Project Tracking. El cual sera el proovedor de 
+ *  Issues (tareas) y Users (desarrolladores) de un proyecto.
+ *  
+ * @author rulo
+ *
+ */
 
+public interface ProjectTracking {
+	
+	public Issue[] getIssues() ;
+	
+	public boolean existUser( String key ) ;
+	
+	public User[] getUsers() ;
+	
+	public boolean existIssue( String key ) ;
+	
 }
