0df79fdab8d797517af46dab5a9f9ff9ff30934e
llezcano <leandro.c.lezcano@gmail.com>
2014-07-27 23:03:10 -0300

    User & Issue interfaces

---
+++ src/main/java/org/tesys/core/project/tracking/Issue.java
@@ -0,0 +1,23 @@
+package org.tesys.core.project.tracking;
+
+public interface Issue {
+	
+	public Integer getKey() ;
+	
+	public Integer getResolutiondate() ;
+	
+	public String getAggregatetimeoriginalestimate();
+	
+	public String getUpdated() ;
+	
+	public String getCreated() ;
+	
+	public String getWorkratio() ;
+	
+	public String getAggregatetimeestimate() ;
+
+	public UserPOJO getReporter() ;
+
+	public IssuePOJO[] getSubtasks() ;
+
+}
---
+++ src/main/java/org/tesys/core/project/tracking/User.java
@@ -0,0 +1,16 @@
+package org.tesys.core.project.tracking;
+
+public interface User {
+	
+	public String getName() ;
+
+
+	public String getEmailAddress() ;
+
+
+	public String getDisplayName() ;
+
+
+	public boolean isActive() ;
+	
+}
