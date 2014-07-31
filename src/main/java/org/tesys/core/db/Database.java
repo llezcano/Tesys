0cd3a894b8a7a5d79d07612f006efcf6be2adfa2
llezcano <leandro.c.lezcano@gmail.com>
2014-07-31 01:58:28 -0300

    estoy re loco

--- src/main/java/org/tesys/core/db/Database.java
+++ src/main/java/org/tesys/core/db/Database.java
@@ -1,5 +1,9 @@
 package org.tesys.core.db;
 
+import java.net.MalformedURLException;
+
+import org.tesys.util.RESTClient;
+
 
 /**
  * SCRUD for data persistence. 
@@ -8,12 +12,25 @@ package org.tesys.core.db;
  */
 public class Database {
     
+    public RESTClient client ;
+   
     public void main( String args[] ) {
 	Database d = new Database() ;
 	d.foo() ;
     }
     
-    public Database() {}
+    public String getURL() {
+	return "http://localhost:8091/" ; 
+    } 
+    
+    public Database() {
+	try {
+	    client = new RESTClient(getURL()) ;
+	} catch (MalformedURLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
     
     public void foo() {
 	
