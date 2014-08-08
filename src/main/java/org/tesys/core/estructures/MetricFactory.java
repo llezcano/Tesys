ae79edecb31c53223d219c17819df5568b071ec1
Leandro <leandro.c.lezcano@gmail.com>
2014-08-08 16:14:42 -0300

    asd

--- src/main/java/org/tesys/core/estructures/MetricFactory.java
+++ src/main/java/org/tesys/core/estructures/MetricFactory.java
@@ -11,29 +11,36 @@ public class MetricFactory {
   
   public static void main(String[] args) {
     new MetricFactory();
+    
+    
   }
   
   public MetricFactory() {
     IValue mul = new Multiplicacion( new SimpleValue("lines"), new SimpleValue("bugs"));
   
-    
+
     Metric lineasPorBugs = new Metric("linbug", "lineas por bugs", 
         "lineas de codigo por cantidad de bugs", "User Made", mul);
     //en db
-    getMetric(lineasPorBugs.toString());
-    
+    //getMetric(lineasPorBugs.toString());
+ 
+    getMetric("{\"class\":\"org.tesys.core.estructures.Multiplicacion\",\"value\":{\"Multiplicacion\":[{\"class\":\"org.tesys.core.estructures.SimpleValue\", \"SimpleValue\":\"lines\"},{\"class\":\"org.tesys.core.estructures.SimpleValue\"\"SimpleValue\":\"bugs\"}]}}") ;
+  
   }
   
   public Metric getMetric(String jsonFormat) {
-    
+    System.out.println(jsonFormat) ;
     ObjectMapper mapper = new ObjectMapper();
     JsonNode actualObj = null;
+    
     try {
+
       actualObj = mapper.readTree(jsonFormat);
-    } catch (IOException e) {}
+    } catch (IOException e) {        }
     
+    System.out.println(actualObj.get( "class" )) ;
     IValue v = getValue(actualObj.get("value"));
-    
+
     
     
     return null;
