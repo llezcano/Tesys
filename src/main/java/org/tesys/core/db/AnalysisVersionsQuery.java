c8914b6f254085ca2f54b080a060b266da5125c7
Leandro <leandro.c.lezcano@gmail.com>
2014-11-22 22:36:21 -0300

    Query for getting Analysis index by date (ascending)

---
+++ src/main/java/org/tesys/core/db/AnalysisVersionsQuery.java
@@ -0,0 +1,47 @@
+package org.tesys.core.db;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.tesys.util.RESTClient;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class AnalysisVersionsQuery implements GenericQuery<List<Long>> {
+
+    protected static final String ES_URL = "http://localhost:9200/";
+    
+    private static final Logger LOG = Logger
+            .getLogger(RevisionByOriginalIdentifierQuery.class.getName());
+
+    
+    @Override
+    public List<Long> execute() {
+        try {
+            RESTClient rc = new RESTClient( ES_URL );
+            JsonNode node =  rc.GET(UriBuilder.fromPath("analysis").path("_mapping").toString()).readEntity( JsonNode.class ) ;
+            Iterator it = node.get( "analysis" ).get( "mappings" ).fieldNames() ;
+            List<Long> dates = new ArrayList<Long>() ;
+            
+            while (it.hasNext()) {
+                dates.add( Long.parseLong( (String)it.next() )  );
+            }
+
+            Collections.sort( dates ) ;
+            return dates ;
+ 
+        } catch (MalformedURLException e) {
+            LOG.log(Level.SEVERE, e.toString(), e);
+        }
+        return new ArrayList<Long>();
+        
+    }
+
+}
